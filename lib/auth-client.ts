40832a913ffcf3a0166d33eb071aec713b92acea
21 Tech Enterprise <59superrichsol@gmail.com>
2024-11-24 17:29:31 +0700

    first commit

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,4 +1,4 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
-  baseURL: "https://www.authapi-six.vercel.app" // the base url of your auth server
+  baseURL: "https://authapi-six.vercel.app" // the base url of your auth server
 })
